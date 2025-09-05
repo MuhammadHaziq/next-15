80f49039bb3ffac8d24e2daf0a09fca952d78027
Muhammad Haziq <muhammadhaziq341@gmail.com>
2025-09-05 16:57:38 +0500

    Update comments route: Implemented cookie handling in the GET method, allowing for theme management. Added commented-out code for potential future use of headers in response cookies.

--- src/app/comments/route.ts
+++ src/app/comments/route.ts
@@ -1,14 +1,35 @@
 import {type NextRequest} from "next/server";
 import { comments } from "./data";
-import { headers } from "next/headers";
+// import { headers } from "next/headers";
+import { cookies } from "next/headers";
+
 export async function GET(request: NextRequest) {
   const { searchParams } = request.nextUrl;
   const searchQuery = searchParams.get("search");
+
+  /** Get Cookies from the request */
+  // const theme = request.cookies.get("theme");
+  // console.log(theme);
+
+  /** Get Cookies from Next.js cookies function */
+  const theme = await cookies()
+  theme.get("theme");
+  console.log(theme);
+  theme.set("theme", "Light");
+  console.log(theme);
+  
   if (searchQuery) {
     const filteredComments = comments.filter((comment) => comment.comment.includes(searchQuery));
     return Response.json(filteredComments);
   }
-  return Response.json(comments);
+  return Response.json(comments, {
+    headers: {
+      "Content-Type": "application/json",
+      /** Set Cookies in the response Using Headers API */
+      /** we are setting cookies using cookies function */
+      // "Set-Cookie": `theme=dark`,
+    },
+  });
 }
 
 export async function POST(request: Request) {
