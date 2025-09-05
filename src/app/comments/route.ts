e8428ba749e19315d1dbe1c296842bea3a8f3be1
Muhammad Haziq <muhammadhaziq341@gmail.com>
2025-09-05 16:41:14 +0500

    Enhance GET and POST routes for comments: Updated GET route to support search functionality and improved POST route with direct header access for authorization checks, ensuring better performance and security.

--- src/app/comments/route.ts
+++ src/app/comments/route.ts
@@ -1,11 +1,27 @@
+import {type NextRequest} from "next/server";
 import { comments } from "./data";
 
-export async function GET(request: Request) {
+export async function GET(request: NextRequest) {
+  const { searchParams } = request.nextUrl;
+  const searchQuery = searchParams.get("search");
+  if (searchQuery) {
+    const filteredComments = comments.filter((comment) => comment.comment.includes(searchQuery));
+    return Response.json(filteredComments);
+  }
   return Response.json(comments);
 }
 
 export async function POST(request: Request) {
   const { comment } = await request.json();
+   /** Both Gives Same Result  */
+      // new Headers(request.headers);
+      // request.headers
+
+  /** Direct access to headers - more efficient for reading */
+  const token = request.headers.get("Authorization");
+  if (token !== "1234567890") {
+    return Response.json({ message: "Unauthorized" }, { status: 401 });
+  }
   const newComment = { id: comments.length + 1, comment };
   comments.push(newComment);
   return Response.json(newComment, {
